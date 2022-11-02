5b11985063f91d4da0a53998f51d03948e1b267f
FilipKufalov <filip.kufalov@hotmail.com>
2022-11-02 03:55:55 +0100

    .

---
+++ FileRename.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkNameChanger
+{
+    public class FileRename
+    {
+        public string OldFileName { get; set; }
+        public string NewFileName { get; set; }
+        public void CheckFileExist()
+        {
+            if (File.Exists(NewFileName))
+            {
+                File.Delete(NewFileName);
+            }
+            File.Move(OldFileName, NewFileName);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using BulkNameChanger;
+
+Console.WriteLine("BulkNameChanger");
+Console.WriteLine("directory path:");
+
+string dir = Console.ReadLine();
+string userFileName;
+string[] allFiles;
+int iterationNumber;
+
+if (dir != null)
+{
+    allFiles = Directory.GetFiles(dir, "*.mp4", SearchOption.TopDirectoryOnly); //Fetch only mp4 files
+
+    Console.Write("\niteration starts from: ");
+    iterationNumber = Convert.ToInt32(Console.ReadLine());
+    Console.Write("what do you want your videos to be named?: ");
+    userFileName = Console.ReadLine();
+
+    foreach (string item in allFiles)
+    {
+        FileRename file = new FileRename();
+        file.OldFileName = item;
+        file.NewFileName = @$"{dir}\{userFileName} {iterationNumber}.mp4";
+        file.CheckFileExist();
+        iterationNumber++;
+        Console.WriteLine(file.NewFileName);
+    }
+} 
+else
+{
+    Console.WriteLine("path directory required!");
+}
